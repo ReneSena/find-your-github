b58f7a27985aa0fbc5909fbc8c6fb3b42c610107
ReneSena <rene.sena3005@gmail.com>
2021-08-06 21:36:32 -0300

    Removed linters

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Hello World</h1>
-		</div>
+	<form noValidate autoComplete="off">
+		<TextField id="outlined-basic" label="Your User" variant="outlined" />
+		<Button type="submit" variant="contained" color="primary">
+			Buscar
+		</Button>
+	</form>
 	);
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+// eslint-disable-next-line no-use-before-define
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
