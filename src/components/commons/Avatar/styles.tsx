e4ccfe76ef454ee971b6e8da633e9fcdb24e035a
ReneSena <rene.sena3005@gmail.com>
2021-08-28 22:45:45 -0300

    Adding styles of the Avatar Component

---
+++ src/components/commons/Avatar/styles.tsx
@@ -0,0 +1,8 @@
+import { Avatar } from '@material-ui/core';
+import styled from 'styled-components';
+
+export const StyledAvatar = styled(Avatar)`
+	width: 120px;
+	height: 120px;
+	margin-bottom: 24px;
+`;
