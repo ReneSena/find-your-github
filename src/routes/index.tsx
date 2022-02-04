f5757fe34343ae7e8d2a6d54a078083865e068cf
ReneSena <rene.sena3005@gmail.com>
2022-02-03 23:19:54 -0300

    Adjusting general

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { HomePage } from '../pages/Home'
+import { HomePage } from '../pages/Home';
 
 export function Routes() {
 	return (
