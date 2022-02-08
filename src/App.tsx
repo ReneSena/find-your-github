58390bd63a96503b7e3dc6e8dc66c07bd7b74f14
ReneSena <rene.sena3005@gmail.com>
2022-02-07 22:37:50 -0300

    Adding reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { CssBaseline, StylesProvider } from "@material-ui/core";
+import { StylesProvider } from "@material-ui/core";
 import { ThemeProvider  } from 'styled-components'
 import React from "react";
 
 import { Routes } from "./routes";
 import { theme } from "./theme/theme";
+import { GlobalStyle } from "./global/global";
 
 const App: React.FC = () => {
 	return (
 		<StylesProvider injectFirst>
 			<ThemeProvider theme={theme}>
-				<CssBaseline />
+				<GlobalStyle />
 				<Routes />
 			</ThemeProvider>
 		</StylesProvider>
